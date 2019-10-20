07deb0743f91a07647c65df4484edf57cd8cfb04
InitusNovus <bigbikky@gmail.com>
2019-10-21 07:44:55 +0900

    no message

---
+++ 0_Src/AppSw/Tricore/AccumulatorManager/AccumulatorManager.c
@@ -0,0 +1,25 @@
+/* 
+ * AccumulatorManager.c
+ * 
+ *  Created on 2019. 10. 21
+ *  Author: Dua
+ * 
+ */
+
+/* Includes */
+#include "AccumulatorManager.h"
+#include "VoltageSensing.h"
+#include "CurrentSensing.h"
+
+/* Function Implementation */
+void AccumualatorManager_init(void)
+{
+    VoltageSensing_init();
+    CurrentSensing_init();
+}
+
+void AccumulatorManager_run_1ms(void)
+{
+    VoltageSensing_run();
+    CurrentSensing_run();
+}
---
+++ 0_Src/AppSw/Tricore/AccumulatorManager/CurrentSensing/CurrentSensing.c
@@ -0,0 +1,74 @@
+/*
+ * CurrensSensing.c
+ * 
+ * Created on 2019. 10. 21
+ * Author: Dua
+ * 
+ */
+
+/* Includes */
+#include "CurrentSensing.h"
+#include "AccumulatorManager.h"
+#include "AdcSensor.h"
+
+/* Macros */
+#define C0_CONST_A  1
+#define C0_CONST_B  0
+#define C1_CONST_A  1
+#define C1_CONST_B  0
+//FIXME: Arbitrary values
+
+/* Data Structures */
+
+
+typedef struct
+{
+    AdcSensor CurrentSensor[2];
+    float32 current;
+}CurrentSensing_t;
+
+
+
+/* Global variables */
+CurrentSensing_t CurrentSensing;
+
+
+/* Private function Prototypes */
+
+/* Function Impementations */
+void CurrentSensing_init(void)
+{
+    AdcSensor_Config config;
+    HLD_Vadc_initChannelConfig(&config.adcConfig);
+
+    config.adcConfig.lpf.config.cutOffFrequency = 1/(2.0*IFX_PI*0.005);
+    config.adcConfig.lpf.config.gain = 1;
+    config.adcConfig.lpf.config.samplingTime = 0.001;
+    config.adcConfig.lpf.activated = TRUE;
+
+    {
+        config.adcConfig.channelIn = &AMS_C0_IN;
+
+        config.tfConfig.a = C0_CONST_A;        
+        config.tfConfig.b = C0_CONST_B;
+        
+        AdcSensor_initSensor(&CurrentSensing.CurrentSensor[0], &config);
+    }
+    {   
+        config.adcConfig.channelIn = &AMS_C1_IN;
+
+        config.tfConfig.a = C1_CONST_A;
+        config.tfConfig.b = C1_CONST_B;
+
+        AdcSensor_initSensor(&CurrentSensing.CurrentSensor[1], &config);
+    }    
+}
+
+
+void CurrentSensing_run(void)
+{
+    for (uint32 i = 0; i < 2; i++)
+    {
+        AdcSensor_getData(&CurrentSensing.CurrentSensor[i]);
+    }
+}
